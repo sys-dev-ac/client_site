ec8c1d90eccd3c1a5adc35efce3d924282015206
Pavan Kumar KN <pavankumarkn715@gmail.com>
2025-10-16 17:02:39 +0530

    add : debounce search

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react'
+import React, {useEffect, useState , useRef} from 'react'
 import axios from "axios";
 
 function Search() {
@@ -10,8 +10,38 @@ function Search() {
 
     const [page , setPage] = useState(1);
 
+    const observer = useRef();
+
+    const [hasMore , setHasMore] = useState(false);
+    const debounceRef = useRef(null);
+
+    // scroll based pagination so we are observing the last element of the array
+    const lastElement = (node) =>{
+        // intersection observer in javascript
+        if(loading) return;
+
+        if(observer.current) {
+            observer.current.disconnect();
+        }
+
+        observer.current = new IntersectionObserver((entries) =>{
+            if(entries[0].isIntersecting && hasMore){
+                setPage((prev) => prev + 1);
+            }
+        });
+
+        if(node){
+            observer.current.observe(node);
+        }
+    }
+
     const handleChange = (e) => {
-        setQuery((e.target.value));
+        if(debounceRef.current) clearTimeout(debounceRef.current);
+
+        debounceRef.current = setTimeout(() =>{
+            setQuery(e.target.value)
+            setPage(1);
+        } , 1000)
     }
 
     useEffect(() => {
@@ -21,24 +51,25 @@ function Search() {
 
     useEffect(() => {
         setLoading(true);
+
         const getSearchItems = async () =>{
-           try{
-            if(query != '') {
-                const data = await axios.get(`https://openlibrary.org/search.json?title=${query}&page=${page}`);
+            try {
+                if(query !== '') {
+                    const data = await axios.get(`https://openlibrary.org/search.json?title=${query}&page=${page}`);
 
-                setLoading(false);
-
-                if(data.data.docs) {
-                    setData((prev) =>{
-                        return [
-                            ...new Set([...prev , ...data.data.docs.map((book) => book.title)])
-                        ]
-                    });
+                    setLoading(false);
+                    setHasMore(data.data.docs.length > 0);
+                    if(data.data.docs) {
+                        setData((prev) =>{
+                            return [
+                                ...new Set([...prev , ...data.data.docs.map((book) => book.title)])
+                            ]
+                        });
+                    }
                 }
             }
-        }
             catch (e) {
-               console.error(e)
+                console.error(e)
             }
         }
 
@@ -46,24 +77,41 @@ function Search() {
 
     }, [query , page]);
 
+
     return (
         <div>
             <input
                 type = "text"
                 onChange={(e) => handleChange(e)}
             />
-
                 {
                     data.map((item , index) => {
-                        return (
-                            <>
-                                <div key = {index}>
-                                    <p>{item}</p>
-                                </div>
-                            </>
+                        if(data.length === index+1){
+                            return (
+                                <>
+                                    <div key = {index} ref = {lastElement}>
+                                        <p>{item}</p>
+                                    </div>
+                                </>
                         )
+                        }
+                        else {
+                            return (
+                                <>
+                                    <div key = {index}>
+                                        <p>{item}</p>
+                                    </div>
+                                </>
+                            )
+                        }
+
                     })
                 }
+
+
+                <div>
+                    {loading && query !== '' && "loading..."}
+                </div>
         </div>
     )
 }
